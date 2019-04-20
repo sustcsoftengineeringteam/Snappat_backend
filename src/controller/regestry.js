6b40da8a95060b76f6668dcf6739f89fc64fc643
RenTao <623059008@qq.com>
2019-04-20 22:49:33 +0800

    补上了registry的文档

--- src/controller/regestry.js
+++ src/controller/regestry.js
@@ -18,12 +18,18 @@ module.exports = class extends think.Controller {
   }
   /**
    * [注册一个新用户]
-   * @para {phone}
-   * @return {success} [成功返回默认success,失败返回fail和error code]
+   * @api {post} /regestry/registry Registry
+   * @apiName Registry
+   * @apiVersion 1.0.1
+   * @apiGroup User
+   * @apiParam {String} username (非必须)用户名
+   * @apiParam {String} phone 电话号码
+   * @apiSuccess {Number} userid 自己的id
    */
   async registryAction() {
     let postdata = this.post();
     let phone = postdata["phone"];
+    let username = postdata["username"];
     // let ans = await SMS_verification(phone);
     let ans = true; //测试期间开放验证
     if (ans) {
@@ -35,8 +41,10 @@ module.exports = class extends think.Controller {
       if (repeat_user.length != 0) {
         return this.fail(1001, Errors["1001"]);
       }
+      if (username == undefined)
+        username = "未命名"
       let add_data = await user.add({
-        "username": "未命名",
+        "username": username,
         "phone": phone,
         "description": "",
         "gender": "男",
